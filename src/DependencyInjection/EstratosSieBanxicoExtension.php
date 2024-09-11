41226ac21478b9c02784a5ee31843c56e905a37a
Julio Rodriguez <julio@estratos.net>
2024-09-11 02:08:42 -0600

    init commit

---
+++ src/DependencyInjection/EstratosSieBanxicoExtension.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Estratos\SieBanxico\DependencyInjection;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
+
+class EstratosSieBanxicoExtension extends Extension
+{
+    public function load(array $configs, ContainerBuilder $container)
+    {
+    }
+}
---
+++ src/EstratosSieBanxico.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Estratos\SieBanxico;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class EstratosSieBanxico extends Bundle
+{
+    
+}
